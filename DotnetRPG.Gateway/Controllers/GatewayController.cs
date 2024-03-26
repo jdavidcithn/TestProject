8d79a3f025f2d25065f0a6c931d80bdb4934eb94
jisaacdavid <julio.david@cit.hn>
2024-03-26 15:41:55 -0600

    completed gateway endpoints.

--- DotnetRPG.Gateway/Controllers/GatewayController.cs
+++ DotnetRPG.Gateway/Controllers/GatewayController.cs
@@ -41,5 +41,78 @@ namespace DotnetRPG.Gateway.Controllers
 
             return Ok(deserializedResponse);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDTO>>> GetCharacter(int id)
+        {
+            var token = _httpContextAccessor.HttpContext!.Session.GetString("AuthToken");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var res = await _httpClient.GetAsync($"https://localhost:7235/api/character/{id}");
+
+            res.EnsureSuccessStatusCode();
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            var dres = JsonConvert.DeserializeObject <ServiceResponse<GetCharacterDTO>>(content);
+
+            return Ok(dres);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDTO>>>> AddCharacter(AddCharacterDTO character)
+        {
+            var token = _httpContextAccessor.HttpContext!.Session.GetString("AuthToken");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var res = await _httpClient.PostAsJsonAsync("https://localhost:7235/api/character", character);
+
+            res.EnsureSuccessStatusCode();
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            var dres = JsonConvert.DeserializeObject<ServiceResponse<List<GetCharacterDTO>>>(content);
+
+            return Ok(dres);
+        }
+
+        [HttpDelete]
+
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDTO>>>> Delete(int id)
+        {
+            var token = _httpContextAccessor.HttpContext!.Session.GetString("AuthToken");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var res = await _httpClient.DeleteAsync($"https://localhost:7235/api/character/{id}");
+
+            res.EnsureSuccessStatusCode();
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            var dres = JsonConvert.DeserializeObject<ServiceResponse<List<GetCharacterDTO>>>(content);
+
+            return Ok(dres);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDTO>>> Update(UpdateCharacterDTO character)
+        {
+            var token = _httpContextAccessor.HttpContext!.Session.GetString("AuthToken");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var res = await _httpClient.PutAsJsonAsync("https://localhost:7235/api/character", character);
+
+            res.EnsureSuccessStatusCode();
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            var dres = JsonConvert.DeserializeObject<ServiceResponse<GetCharacterDTO>>(content);
+
+            return dres;
+        }
     }
 }
